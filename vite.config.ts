e9a0c6706046f41db0808275937fe319e5f19acf
Aditi <maditi2406@gmail.com>
2025-07-24 15:51:21 +0530

    Fix added base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/smart-waste-management-website/",
   server: {
     host: "::",
     port: 8080,
